1a1b1f037ac6a0787d601e2e2565525812273678
Rodrigo Urban <rorikstark@gmail.com>
2019-08-14 19:38:49 -0300

    make it already

--- server/settings.py
+++ server/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.contrib.messages.middleware.MessageMiddl-eware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
