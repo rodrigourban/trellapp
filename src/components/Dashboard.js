d1dca606cb152dfce1912098950dcccc6d27997e
Rodrigo Urban <rorikstark@gmail.com>
2019-08-16 00:37:05 -0300

    make it stronger

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -28,7 +28,7 @@ class Dashboard extends React.Component {
           }>
             <div style={{ fontSize: '16px', lineHeight: '20px', color: '#fffff', fontWeight: '700', color: 'rgb(255,255,255)' }}>{board.title}</div>
           </div>
-          <div onClick={() => this.deleteBoard(board.id)} style={{ width: '30px', position: 'absolute', right: '-7px', top: '7px' }} >...</div>
+          <div onClick={() => this.deleteBoard(board.id)} style={{ width: '30px', position: 'absolute', right: '-7px', top: '7px' }} >x</div>
         </NavLink >
       )
     })
