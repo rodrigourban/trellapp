909f50a6e050f7e08228ceefb1b41bfb77289881
Rodrigo Urban <rorikstark@gmail.com>
2019-08-16 00:26:23 -0300

    make it stronger

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -20,6 +20,7 @@ class Dashboard extends React.Component {
     const content = this.props.boards.map((board) => {
       return (
         <NavLink to={`/board/${board.id}`} className="dashboard-board" style={{
+          position: 'relative',
           height: '100px', backgroundColor: board.background, borderRadius: '5px',
           padding: '5px', textDecoration: 'none', color: '#fffff'
         }}>
@@ -27,7 +28,7 @@ class Dashboard extends React.Component {
           }>
             <div style={{ fontSize: '16px', lineHeight: '20px', color: '#fffff', fontWeight: '700', color: 'rgb(255,255,255)' }}>{board.title}</div>
           </div>
-          <div onClick={() => this.deleteBoard(board.id)} style={{ width: '30px', position: 'relative', right: '-10px', top: '-5px' }} >...</div>
+          <div onClick={() => this.deleteBoard(board.id)} style={{ width: '30px', position: 'absolute', right: '-10px', top: '-5px' }} >...</div>
         </NavLink >
       )
     })
